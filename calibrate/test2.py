990982901793cb070783b8da56aff3c95ed9906a
Mackeprang <hr_mackeprang@hotmail.com>
2018-09-11 09:41:34 +0200

    added essentialmatrix and canvas to test2

--- calibrate/test2.py
+++ calibrate/test2.py
@@ -39,7 +39,7 @@ def draw_flow(img,p1,p2,mask=None):
             for i, (new, old) in enumerate(zip(p1_outliers, p2_outliers)):
                 a, b = new.ravel()
                 c, d = old.ravel()
-                img = cv2.line(img, (a, b), (c, d), (0, 0, 255), 1)
+                img = cv2.line(img, (a, b), (c, d), (0, 0, 255), 1.5)
     return img
 
 def sparse_optical_flow(img1,img2,points,fb_threshold,optical_flow_params):
@@ -55,7 +55,7 @@ def sparse_optical_flow(img1,img2,points,fb_threshold,optical_flow_params):
     return new_points,old_points
 
 def update_motion(points1,points2,Rpos,tpos,cam_mat=None,scale = 1.0):
-    E, mask = cv2.findEssentialMat(points1,points2,cameraMatrix=cam_mat,method=outlier_alg,prob=0.999, mask=None)
+    E, mask = cv2.findEssentialMat(points1,points2,cameraMatrix=cam_mat,method=cv2.LMEDS,prob=0.999, mask=None)
     newmask = np.copy(mask)
     _,R,t,newmask = cv2.recoverPose(E,points1,points2,cameraMatrix=cam_mat,mask=newmask)
     tp = tpos+np.dot(Rpos,t)*scale
@@ -73,7 +73,7 @@ def find_features(im,method=0):
         points.append(point.pt)
     points = np.array(points, dtype=np.float32)
 
-    return np.reshape(points, (-1, 1, 2))
+    return np.reshape(points, (-1, 1, 2)),keypoints
 
 def preprocess_image(im,size=(640,480),method=0):
     im = cv2.resize(im, size)
@@ -85,19 +85,46 @@ def preprocess_image(im,size=(640,480),method=0):
         res = clahe.apply(gray)
     return res
 
+def draw_path(canvas,points,scale=0.1,clear=True,color=(0,255,0)):
+    points = np.reshape(points,(-1,3))
+    dim = np.shape(canvas)
+    canvas_h = dim[0]
+    canvas_w = dim[1]
+    canvas = np.zeros(dim,dtype=np.uint8)
+
+    pos_orig = points[0]
+    x_orig = pos_orig[0]
+    y_orig = pos_orig[1]
+
+    for i, p2 in enumerate(points):
+        p1 = points[i-1]
+        x1 = int((p1[0]-x_orig)*scale+canvas_w/2)
+        y1 = int((p1[1] - y_orig) * scale + canvas_h / 2)
+        x2 = int((p2[0] - x_orig) * scale + canvas_w / 2)
+        y2 = int((p2[1] - y_orig) * scale + canvas_h / 2)
+        cv2.line(canvas,(x1,y1),(x2,y2),color,1)
+    return canvas
+
+cam_mat = np.array([[1417.84363, 0.0, 353.360213], [0.0, 1469.27135, 270.122002],  [0.0, 0.0, 1.0]], dtype=np.float32)
 im_filename = '*.png'
 imdir = '/Users/Mackeprang/Dropbox (Personlig)/Master Thesis/Pictures/20181005_084733.9640_Mission_1'
 #imdir = '/Users/Mackeprang/Dropbox (Personlig)/Master Thesis/Pictures/20181005_084733.9640_Mission_1'
-imdir = 'C:/Users/Rasmus/Dropbox/Master Thesis/Pictures/20181005_084733.9640_Mission_1'
+#imdir = 'C:/Users/Rasmus/Dropbox/Master Thesis/Pictures/20181005_084733.9640_Mission_1'
 filenames = []
 images = []
+path = []
 timestamp = []
-backward_flow_threshold = 1
+backward_flow_threshold = 3
 prev_frame = None
+canvas = np.zeros((640, 480, 3), dtype=np.uint8)
 select = 2
 filepath = ''.join((imdir,'/',im_filename))
-optical_flow_params = dict(winSize = (30,30),
-                           maxLevel = 3,
+fast_params = dict(threshold = 30,
+                   nonmaxSuppression=True,
+                   type=cv2.FAST_FEATURE_DETECTOR_TYPE_7_12)
+
+optical_flow_params = dict(winSize = (10,10),
+                           maxLevel = 4,
                            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.01))
 blob_params = dict(minThreshold = 10,
                    maxThreshold = 200,
@@ -123,7 +150,7 @@ params = cv2.SimpleBlobDetector_Params()
 
 # Change thresholds
 params.minThreshold = 100;
-params.maxThreshold = 200;
+params.maxThreshold = 300;
 
 # Filter by Area.
 params.filterByArea = True
@@ -131,7 +158,7 @@ params.minArea = 10
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.
+params.minCircularity = 0.1
 
 # Filter by Convexity
 params.filterByConvexity = True
@@ -142,35 +169,41 @@ params.filterByInertia = True
 params.minInertiaRatio = 0.1
 prev_points = []
 feat_method = 0
+Rpos = np.eye(3,3,dtype=np.float32)
+tpos = np.zeros((3,1),dtype=np.float32)
 blob = cv2.SimpleBlobDetector_create(params)
-fast = cv2.FastFeatureDetector_create()
+fast = cv2.FastFeatureDetector_create(**fast_params)
 for i,frame in enumerate(filenames):
     if image_broken(frame):
         continue
 
-    gray = preprocess_image(cv2.imread(frame),(640,480),1)
+    gray = preprocess_image(cv2.imread(frame),(640,480),0)
     im = cv2.resize(cv2.imread(frame),(640,480))
     if prev_frame is None:
-        prev_points = find_features(gray,0)
+        prev_points,keypoints = find_features(gray,0)
         prev_frame = gray.copy()
         continue
 
-    if len(prev_points) < 100:
-        prev_points = find_features(gray, feat_method)
-
+    if len(prev_points) < 100000:
+        prev_points,keypoints = find_features(prev_frame, feat_method)
+    print "Number of features: " + str(len(prev_points))
     new_points, prev_points = sparse_optical_flow(prev_frame, gray, prev_points, backward_flow_threshold,
                                                   optical_flow_params)
-
+    path.append(tpos)
+    old_tpos = np.copy(tpos)
+    #Rpos, tpos, mask = update_motion(prev_points, new_points, Rpos, old_tpos, cam_mat)
     flow_im = draw_flow(im,prev_points,new_points)
-    #im_combined = np.hstack((im, gray))
-    cv2.imshow('Recordings: Mission 1',flow_im)
+    #canvas = draw_path(canvas,path)
+    gray_with_kp = cv2.drawKeypoints(gray,keypoints,np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    im_combined = np.hstack((flow_im, gray_with_kp))
+    cv2.imshow('Recordings: Mission 1',im_combined)
     prev_frame = gray.copy()
     prev_points = new_points.copy()
     if cv2.waitKey(100) & 0xFF  == ord('q'): #Pause
         key = cv2.waitKey()
         if cv2.waitKey() & 0xFF  == ord('q'): #Exiting
             break
-        elif cv2.waitKey() & 0xFF  == ord('1'): #Exiting
+        elif cv2.waitKey() & 0xFF  == ord('1'):
             feat_method = 1
         elif cv2.waitKey() & 0xFF  == ord('2'): #Using
             feat_method = 0
