fd8711e43353bb09bfc0ea1054afad505c2c034d
Mackeprang <hr_mackeprang@hotmail.com>
2018-09-14 15:51:40 +0200

    plotting acc data

--- calibrate/read_h5_data.py
+++ calibrate/read_h5_data.py
@@ -1,7 +1,8 @@
 import h5py
 import matplotlib.pyplot as plt
-
-filename = "/Users/Mackeprang/Dropbox (Personlig)/Master Thesis/Data/20180910 Optical flowtest/20181010_105618_Mission_1/output.h5"
+import utm
+import numpy as np
+filename = "/Users/Mackeprang/Dropbox (Personlig)/Master Thesis/Data/20180910 Optical flowtest/20181010_122400_Mission_5/output.h5"
 
 f = h5py.File(filename,'r')
 
@@ -11,13 +12,37 @@ depth = f["Position"]["Depth"]
 acc_x = f["Position"]["Acc_X"]
 acc_y = f["Position"]["Acc_Y"]
 acc_z = f["Position"]["Acc_Z"]
+lat = f["Position"]["Lat"]
+lon = f["Position"]["Lon"]
 
-plt.plot(acc_x)
+#dx = (lon2-lon)*40000*math.cos((lat1+lat2)*math.pi/360)/360
+#dy = (lat1-lat2)*40000/360
+xy = []
+for x,y in zip(lat,lon):
+    xy.append(utm.from_latlon(x,y))
+
+rel_pos_x = []
+rel_pos_y = []
+for pos in xy:
+    rel_pos_x.append(pos[0]-xy[0][0])
+    rel_pos_y.append(pos[1] - xy[0][1])
+plt.close("all")
+fig1 = plt.figure()
+thr = np.ones_like(alt)
+plt.scatter(rel_pos_x,rel_pos_y)
+fig2 = plt.figure()
+thr = np.ones_like(alt)
+thr *= 1.6
+plt.plot(alt)
+plt.plot(thr)
+fig3 = plt.figure()
 plt.plot(acc_y)
+plt.plot(acc_x)
 plt.plot(acc_z)
 ax = plt.gca()
+ax.legend(["Acc Y","Acc X", "Acc Z"])
 plt.ylabel("Altitude")
 ax.grid()
 plt.show()
 
-# plt.close("all")
+plt.close("all")
