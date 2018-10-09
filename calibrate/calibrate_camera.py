eb715d0ebc3ac03479dd7d76f9f23919e9873487
Mackeprang <hr_mackeprang@hotmail.com>
2018-10-09 11:29:49 +0200

    new calibration data

--- calibrate/calibrate_camera.py
+++ calibrate/calibrate_camera.py
@@ -16,11 +16,11 @@ import glob
 from sys import argv
 import os
 import shutil
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+#from picamera import PiCamera
+#from picamera.array import PiRGBArray
 import time
 
-images_path = "calib_data/"
+images_path = "/Users/Mackeprang/Dropbox (Personlig)/Master Thesis/Calibration/"
 create_images = False
 for i, argument in enumerate(argv):
     if argument in '--images_path':
@@ -40,39 +40,36 @@ for i, argument in enumerate(argv):
                 os.makedirs(images_path)
 
 if create_images:
-    camera_resolution = (640 / 2, 480 / 2)
-    with PiCamera(resolution=camera_resolution, framerate=24) as camera:
-        with PiRGBArray(camera, size=camera_resolution) as stream:
-            time.sleep(2)
-            for frame in camera.capture_continuous(stream, format='bgr', use_video_port=True):
-                stream.truncate()
-                stream.seek(0)
-                if img_count is -1:
-                    img_count += 1
-                    continue
-                img = frame.array
-                img = cv2.flip(img, -1)
-                cv2.imshow("Calibrate Image", img)
-                # if cv2.waitKey(20) == 32: # Space
-                time.sleep(2)
+    camera_resolution = (800,600)
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800);
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600);
+    img_count = 0
+    while True:
+        ret, frame = cap.read()
+        if ret == True:
+            #frame = cv2.resize(frame,camera_resolution)
+            cv2.imshow("Calibrate Image", frame)
+            if cv2.waitKey(20) == 32:  # Space
                 img_count += 1
                 print("Image number: {}".format(img_count))
                 img_name = images_path + "img%.2d" % img_count + ".png"
-                cv2.imwrite(img_name, img)
-                stream.truncate(0)
-                if img_count == 60 or cv2.waitKey(20) == ord('q'):
-                    break
+                cv2.imwrite(img_name, frame)
+
+            if img_count == 60 or cv2.waitKey(20) == ord('q'):
+                break
+
 
 cv2.destroyAllWindows()
-
+#
 if images_path is not None:
     ### CONFIG ###
     # image dimensions
 
-    scale = 25.4  # [mm] real world size of checker board squares (1 inch)
+    scale = 30  # [mm] real world size of checker board squares (1 inch)
     # number of chessboard squares:
-    chessboard_w = 6
-    chessboard_h = 9
+    chessboard_w = 5
+    chessboard_h = 7
     ##############
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -85,26 +82,26 @@ if images_path is not None:
 
     images = glob.glob(''.join((images_path, '*.png')))
     imshape = None
+    print len(images)
     for i, fname in enumerate(images):
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         imshape = gray.shape
-
-        ret, corners = cv2.findChessboardCorners(gray, (chessboard_h, chessboard_w), None)
-
+        ret, corners = cv2.findChessboardCorners(gray, (chessboard_h, chessboard_w))
         if ret is True:
             objpoints.append(objp)
             cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners)
-            print(i)
+            # print(i)
             # cv2.drawChessboardCorners(img, (chessboard_h, chessboard_w), corners, ret)
             # fname_new = fname[0:-4]
             # cv2.imshow("img", img)
-            # cv2.imwrite(fname_new, img)
+            # #cv2.imwrite(fname_new, img)
             # print "Img number: %d" % i+1
             # key = cv2.waitKey(100) & 0xFF
 
     cv2.destroyAllWindows()
+    print np.shape(images)
     ret, cam_mat, distcoeff, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imshape[::-1], None, None)
     save_mat = raw_input("Save camera matrix and distortions parameters: ")
     if save_mat is "y":
